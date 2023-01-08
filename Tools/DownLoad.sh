6a9017831ec31ffabf24abf3969237aaf60320d0
snfan <625726371@qq.com>
2023-01-08 21:53:39 +0800

    Update DownLoad.sh

--- Tools/DownLoad.sh
+++ Tools/DownLoad.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+cd WebRoot/Tools
+
 curl -L -k -o zlib-1.2.12.zip https://github.com/libarchive/zlib/archive/v1.2.12.zip
 curl -L -k -o bzip2-1ea1ac188ad4b9cb662e3f8314673c63df95a589.zip https://github.com/libarchive/bzip2/archive/1ea1ac188ad4b9cb662e3f8314673c63df95a589.zip
 curl -L -k -o xz-5.2.5.zip https://github.com/libarchive/xz/archive/v5.2.5.zip
 curl -L -k -o zstd-1.5.2.zip https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.zip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd ../../
